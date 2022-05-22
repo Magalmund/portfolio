3b845902cd362b97742eab5a3714418c5ce96a05
Kaloid <poker6-96@mail.ru>
2022-05-22 23:04:17 +0300

    change

---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './about.css'
+
+
+const about = () => {
+	return (
+		<section id="about">about</section>
+	)
+}
+
+export default about
---
+++ src/components/services/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './service.css'
+
+const Services = () => {
+	return (
+		<section id="services">Services</section>
+	)
+}
+
+export default Services
