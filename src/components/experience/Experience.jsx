80aa81482fec79c4b39aab3dbf6cc17c52d96446
Kaloid <poker6-96@mail.ru>
2022-05-23 15:59:05 +0300

    change

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,9 +1,88 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from 'react-icons/bs'
 
 const Experience = () => {
 	return (
-		<section id="experience">Experience</section>
+		<section id="experience">
+			<h5>What skills I have</h5>
+			<h2>My experience</h2>
+
+			<div className="container experience_container">
+				<div className="experience_frontend">
+					<h3>Frontend development</h3>
+					<div className="experience_content">
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>HTML</h4>
+							<small className='text-light'>Experienced</small>
+						</article>
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>CSS</h4>
+							<small className='text-light'>Intermediate</small>
+						</article>
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>Javascript</h4>
+							<small className='text-light'>Experienced</small>
+						</article>
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>Tailwind</h4>
+							<small className='text-light'>Experienced</small>
+						</article>
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>React</h4>
+							<small className='text-light'>Experienced</small>
+						</article>
+
+					</div>
+				</div>
+				<div className="experience_backend">
+					<h3>Backend development</h3>
+					<div className="experience_content">
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>Node JS</h4>
+							<small className='text-light'>Experienced</small>
+						</article>
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>Mongo DB</h4>
+							<small className='text-light'>Intermediate</small>
+						</article>
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>PHP</h4>
+							<small className='text-light'>Intermediate</small>
+						</article>
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>MySQL</h4>
+							<small className='text-light'>Experienced</small>
+						</article>
+
+						<article className='experience_details'>
+							<BsPatchCheckFill/>
+							<h4>Python</h4>
+							<small className='text-light'>Experienced</small>
+						</article>
+
+					</div>
+				</div>
+			</div>
+		</section>
 	)
 }
 
